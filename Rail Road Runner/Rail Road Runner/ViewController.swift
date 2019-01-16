3b5ed8fd88da104c0ecbe73f820efefe051158b9
Martin1248 <martinonlinedrive@gmail.com>
2019-01-16 12:56:54 +0100

    Added PickerView, Dismiss Keyboard, disable button, accurracy check and more

--- Rail Road Runner/Rail Road Runner/ViewController.swift
+++ Rail Road Runner/Rail Road Runner/ViewController.swift
@@ -10,8 +10,9 @@ import UIKit
 import CoreLocation
 import AVFoundation
 
-class ViewController: UIViewController, CLLocationManagerDelegate {
+class ViewController: UIViewController, CLLocationManagerDelegate, UIPickerViewDataSource, UIPickerViewDelegate {
 
+    let trainLengths = ["100", "150", "200", "250", "300", "350", "400", "450", "500", "550", "600", "650", "700", "750" , "800" , "850", "900", "950", "1000"]
     let userDefaultsTrainLength = "trainLength"
 
     @IBOutlet weak var trainLengthTextBox: UITextField!
@@ -28,12 +29,16 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        initLocationManager()
-        locationManager.startUpdatingLocation()
-    }
+        let pickerView = UIPickerView()
+        pickerView.delegate = self
+        trainLengthTextBox.inputView = pickerView
 
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
+        initLocationManager()
+        if CLLocationManager.locationServicesEnabled() {
+            locationManager.startUpdatingLocation()
+        } else {
+            setButtonEnabled(enabled: false, disabledReason: "Ortungs Service ist nicht aktiviert")
+        }
 
         if let trainLenght = UserDefaults.standard.string(forKey: userDefaultsTrainLength) {
             trainLengthTextBox.text = trainLenght;
@@ -41,7 +46,12 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
 
         startStopButton.layer.cornerRadius = 49
         startStopButton.clipsToBounds = true
-        startStopButton.isEnabled = true
+
+
+        start(start: false)
+
+        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(ViewController.dismissKeyboard))
+        view.addGestureRecognizer(tap)
     }
 
     override func viewWillDisappear(_ animated: Bool) {
@@ -61,22 +71,22 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
         let currentLocation:CLLocation = locations[0] as CLLocation
 
+        if currentLocation.horizontalAccuracy < 50 {
+            setButtonEnabled(enabled: true, disabledReason: "")
+        } else {
+            if started {
+                setButtonEnabled(enabled: false, disabledReason: "Abbruch: Koordinaten sind zu ungenau")
+                start(start: false)
+            } else {
+                setButtonEnabled(enabled: false, disabledReason: "Koordinaten sind zu ungenau")
+            }
+        }
+
         if started {
             if startLocation == nil {
                 startLocation = currentLocation
             } else {
-                let currentDistance : Int! = Int(startLocation!.distance(from: currentLocation))
-                let expectedLength : Int! = Int(trainLengthTextBox.text!)
-                var remainingLength = expectedLength  - currentDistance
-                if remainingLength < 0 {
-                    remainingLength = 0
-                    started = false
-                    trainLengthTextBox.isUserInteractionEnabled = true
-                    startStopButton.setTitle("Start", for: UIControl.State.normal)
-                    AudioServicesPlaySystemSound(kSystemSoundID_Vibrate)
-                    AudioServicesPlaySystemSound(1010)
-                }
-                remainingTrainLength.text = String(remainingLength)
+                newLocationReceived(newLocation: currentLocation)
             }
         } else {
             startLocation = nil
@@ -84,26 +94,82 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
     }
 
     func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
-        print("Unable to access your current location: " + error.localizedDescription)
+        if started {
+            setButtonEnabled(enabled: false, disabledReason: "Abbruch: Koordinaten sind nicht mehr verfügbar")
+            start(start: false)
+        } else {
+            setButtonEnabled(enabled: false, disabledReason: "Koordinaten sind nicht verfügbar")
+        }
     }
 
-    // MARK: - UI
+    // MARK: - Picker View
 
-    @IBAction func startStopWasPressed(_ sender: Any) {
-         if Int(trainLengthTextBox.text ?? "0") == 0 {
-            return
-        }
+    func numberOfComponentsInPickerView(pickerView: UIPickerView) -> Int {
+        return 1
+    }
 
-        if !started {
-            started = true
-            trainLengthTextBox.isUserInteractionEnabled = false
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return trainLengths.count
+    }
+
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return trainLengths[row]
+    }
+
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        trainLengthTextBox.text = trainLengths[row]
+    }
+
+    // MARK: - Application logic
+
+    @objc func dismissKeyboard() {
+        view.endEditing(true)
+    }
+
+    @IBAction func buttonWasPressed(_ sender: Any) {
+         start(start: !started)
+    }
+
+    func start(start: Bool) {
+        started = start
+        trainLengthTextBox.isUserInteractionEnabled = start ? false : true
+        startStopButton.setTitle(start ? "Stop" : "Start", for: UIControl.State.normal)
+
+        if start {
             UserDefaults.standard.set(trainLengthTextBox.text, forKey: userDefaultsTrainLength)
-            startStopButton.setTitle("Stop", for: UIControl.State.normal)
         } else {
-            started = false
-            trainLengthTextBox.isUserInteractionEnabled = true
-            startStopButton.setTitle("Start", for: UIControl.State.normal)
+            progressView.progress = 0
+            remainingTrainLength.text = ""
         }
+    }
+
+    func newLocationReceived(newLocation : CLLocation) {
+        let currentDistance : Int! = Int(startLocation!.distance(from: newLocation))
+        let expectedLength : Int! = Int(trainLengthTextBox.text!)
+        let remainingLength = expectedLength - currentDistance
+        if remainingLength > 0 {
+            let fractionalProgress : Float = Float(currentDistance) / Float(expectedLength);
+            progressView.progress = fractionalProgress;
+            remainingTrainLength.text = String(remainingLength)
+        } else {
+            start(start: false)
+            AudioServicesPlaySystemSound(kSystemSoundID_Vibrate)
+            //AudioServicesPlaySystemSound(1010)
+        }
+    }
+
+    func setButtonEnabled(enabled: Bool, disabledReason: String) {
+        startStopButton.isEnabled = enabled
+        if enabled {
+            startStopButton.backgroundColor = UIColor(red: 0.8, green: 0.1, blue: 0.1, alpha: 0.5)
+        } else {
+            startStopButton.backgroundColor = UIColor(red: 0.1, green: 0.1, blue: 0.1, alpha: 0.5)
+        }
+        print(disabledReason)
     }
 }
 
